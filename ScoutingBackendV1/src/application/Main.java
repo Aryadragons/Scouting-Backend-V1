f4c0e9a22c60cec3b9078270958b62ddb17d27b7
Arydragons <sunnybgp.icloud.com>
2024-07-03 15:14:10 -0500

    made it so you have to click on menu items for it's tab to appear

--- ScoutingBackendV1/src/application/Main.java
+++ ScoutingBackendV1/src/application/Main.java
@@ -16,7 +16,7 @@ import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
 
 
-public class Main extends Application {
+public class Main extends Application implements EventHandler<ActionEvent>{
 	
 	// Looking Up Data Menu Items
 	private MenuItem searchTeams;
@@ -48,6 +48,7 @@ public class Main extends Application {
 	private MenuItem exportScDataMI;
 	private MenuItem exportDriveTeamDataMI;
 	private MenuItem exportDriveTeamComsMI;
+	private TabPane mainTP;
 	//Home page Menu Item
 	private MenuItem homePageMI;
 	@Override
@@ -60,41 +61,8 @@ public class Main extends Application {
 			MenuBar MB = createMenuBar();
 			root.setTop(MB);
 			//adding the Tab panes
-			TabPane mainTP = new TabPane();
+			mainTP = new TabPane();
 			root.setBottom(mainTP);
-			//adding search tabs
-			searchTeamsTab seTeams = new searchTeamsTab();
-			Tab seTeamsTab = new Tab("Search Teams", seTeams);
-			searchAllincesTab seAllinces = new searchAllincesTab();
-			Tab seAllincesTab = new Tab("Search Allinces", seAllinces);
-			searchAutosTab seAutos = new searchAutosTab();
-			Tab seAutosTab = new Tab("Search Autos", seAutos);
-			//adding compare tabs
-			compareTeamsTab comTeams = new compareTeamsTab();
-			Tab comTeamsTab = new Tab("Compare Teams", comTeams);
-			compareAllincesTab comAllinces = new compareAllincesTab();
-			Tab comAllincesTab = new Tab("Compare Allinces", comAllinces);
-			compareAutosTab comAutos = new compareAutosTab();
-			Tab comAutosTab = new Tab("Compare Autos", comAutos);
-			//adding findBest Tabs
-			findBestTeamTab findTeam = new findBestTeamTab();
-			Tab findTeamTab = new Tab("Find Best Team", findTeam);
-			findBestAllinceTab findAllince = new findBestAllinceTab();
-			Tab findAllinceTab = new Tab("Find Best Allince", findAllince);
-			findBestAutoTab findAuto = new findBestAutoTab();
-			Tab findAutoTab = new Tab("Find Best auto", findAuto);
-			findBestStatTab findStat = new findBestStatTab();
-			Tab findStatTab = new Tab("Find Best Stat", findStat);
-			findBestRobotArchatype findArchatype = new findBestRobotArchatype();
-			Tab findArchatypeTab = new Tab("Find Best Robot Archatype", findArchatype);
-			//adding create Tabs
-			createAllinceTab creAllince = new createAllinceTab();
-			Tab creAllinceTab = new Tab("Create Allince", creAllince);
-			createMatchTab creMatch = new createMatchTab();
-			Tab creMatchTab = new Tab("Create Match", creMatch);
-			createAutoPlanTab creAutoPlan = new createAutoPlanTab();
-			Tab creAutoPlanTab = new Tab("Create Auto Plan", creAutoPlan);
-			mainTP.getTabs().addAll(seTeamsTab, seAllincesTab, seAutosTab, comTeamsTab, comAllincesTab, comAutosTab, findTeamTab, findAllinceTab, findAutoTab, findStatTab, findArchatypeTab, creAllinceTab, creMatchTab, creAutoPlanTab);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -115,34 +83,70 @@ public class Main extends Application {
 		Menu editMenu = new Menu("Edit Data");
 		Menu exportMenu = new Menu("Export Data");
 		// Creating the Menu Items
+		//home page
 		homePageMI = new MenuItem("Home");
+		homePageMI.setOnAction(this);
+		//search stuff
 		searchTeams = new MenuItem("Teams");
+		searchTeams.setOnAction(this);
 		searchAllince = new MenuItem("Allinces");
+		searchAllince.setOnAction(this);
 		searchAutos = new MenuItem("Autos");
+		searchAutos.setOnAction(this);
+		//compare stuff
 		compareTeams = new MenuItem("Teams");
+		compareTeams.setOnAction(this);
 		compareAllince = new MenuItem("Allinces");
+		compareAllince.setOnAction(this);
 		compareAutos = new MenuItem("Autos");
+		compareAutos.setOnAction(this);
+		//find best stuff
 		findBestTeam = new MenuItem("Team");
+		findBestTeam.setOnAction(this);
 		findBestAllince = new MenuItem("Allince");
+		findBestAllince.setOnAction(this);
 		findBestAuto = new MenuItem("Auto");
+		findBestAuto.setOnAction(this);
 		findBestStat = new MenuItem("Stat");
+		findBestStat.setOnAction(this);
 		findBestRobotArchatype = new MenuItem("Robot Archatype");
+		findBestRobotArchatype.setOnAction(this);
+		//create stuff
 		createAllince = new MenuItem("Allince");
+		createAllince.setOnAction(this);
 		createMatch = new MenuItem("Match");
+		createAllince.setOnAction(this);
 		createAutoplan = new MenuItem("Auto Plan");
+		createAutoplan.setOnAction(this);
 		createMacthPlan = new MenuItem("Macth Plan");
+		createMacthPlan.setOnAction(this);
+		//open stuff
 		openNewScDataMI = new MenuItem("Scouting Data");
+		openNewScDataMI.setOnAction(this);
 		openNewPitDataMI = new MenuItem("Pit Data");
+		openNewPitDataMI.setOnAction(this);
 		openNewDriveTeamDataMI = new MenuItem("Drive Team Data");
+		openNewDriveTeamDataMI.setOnAction(this);
 		openNewDriveTeamComsMI = new MenuItem("Drive Team Comments");
+		openNewDriveTeamComsMI.setOnAction(this);
+		//edit stuff
 		editScDataMI = new MenuItem("Scouting Data");
+		editScDataMI.setOnAction(this);
 		editPitDataMI = new MenuItem("Pit Data");
+		editPitDataMI.setOnAction(this);
 		editDriveTeamDataMI = new MenuItem("Drive Team Data");
+		editDriveTeamDataMI.setOnAction(this);
 		editDriveTeamComsMI = new MenuItem("Drive Team Comments");
+		editDriveTeamComsMI.setOnAction(this);
+		//export stuff
 		exportScDataMI = new MenuItem("Scouting Data");
+		exportScDataMI.setOnAction(this);
 		exportPitDataMI = new MenuItem("Pit Data");
+		exportPitDataMI.setOnAction(this);
 		exportDriveTeamDataMI = new MenuItem("Drive Team Data");
+		exportDriveTeamDataMI.setOnAction(this);
 		exportDriveTeamComsMI = new MenuItem("Drive Team Comments");
+		exportDriveTeamComsMI.setOnAction(this);
 		//Adding Menu Items to their Menus
 		homePageMenu.getItems().addAll(homePageMI);
 		searchMenu.getItems().addAll(searchTeams, searchAutos, searchAllince);
@@ -157,7 +161,233 @@ public class Main extends Application {
 		return mB;
 	}
 	
+	private void makeSearchTeams() {
+		searchTeamsTab seTeams = new searchTeamsTab();
+		Tab seTeamsTab = new Tab("Search Teams", seTeams);
+		mainTP.getTabs().addAll(seTeamsTab);
+	}
+	
+	private void makeSearchAllinces() {
+		searchAllincesTab seAllinces = new searchAllincesTab();
+		Tab seAllincesTab = new Tab("Search Allinces", seAllinces);
+		mainTP.getTabs().addAll(seAllincesTab);
+	}
+	
+	private void makeSearchAutos() {
+		searchAutosTab seAutos = new searchAutosTab();
+		Tab seAutosTab = new Tab("Search Autos", seAutos);
+		mainTP.getTabs().addAll(seAutosTab);
+	}
+	
+	private void makeCompareTeams() {
+		compareTeamsTab comTeams = new compareTeamsTab();
+		Tab comTeamsTab = new Tab("Compare Teams", comTeams);
+		mainTP.getTabs().addAll(comTeamsTab);
+	}
+	
+	private void makeCompareAllinces() {
+		compareAllincesTab comAllinces = new compareAllincesTab();
+		Tab comAllincesTab = new Tab("Compare Allinces", comAllinces);
+		mainTP.getTabs().addAll(comAllincesTab);
+	}
+	
+	private void makeCompareAutos() {
+		compareAutosTab comAutos = new compareAutosTab();
+		Tab comAutosTab = new Tab("Compare Autos", comAutos);
+		mainTP.getTabs().addAll(comAutosTab);
+	}
+	
+	private void makeFindBestTeam() {
+		findBestTeamTab findTeam = new findBestTeamTab();
+		Tab findTeamTab = new Tab("Find Best Team", findTeam);
+		mainTP.getTabs().addAll(findTeamTab);
+	}
+	
+	private void makeFindBestAllince() {
+		findBestAllinceTab findAllince = new findBestAllinceTab();
+		Tab findAllinceTab = new Tab("Find Best Allince", findAllince);
+		mainTP.getTabs().addAll(findAllinceTab);
+	}
+	
+	private void makeFindBestAuto() {
+		findBestAutoTab findAuto = new findBestAutoTab();
+		Tab findAutoTab = new Tab("Find Best auto", findAuto);
+		mainTP.getTabs().addAll(findAutoTab);
+	}
+	
+	private void makeFindBestStat() {
+		findBestStatTab findStat = new findBestStatTab();
+		Tab findStatTab = new Tab("Find Best Stat", findStat);
+		mainTP.getTabs().addAll(findStatTab);
+	}
+	
+	private void makeFindBestRobotArchatype() {
+		findBestRobotArchatype findArchatype = new findBestRobotArchatype();
+		Tab findArchatypeTab = new Tab("Find Best Robot Archatype", findArchatype);
+		mainTP.getTabs().addAll(findArchatypeTab);
+	}
+	
+	private void makeCreateAllince() {
+		createAllinceTab creAllince = new createAllinceTab();
+		Tab creAllinceTab = new Tab("Create Allince", creAllince);
+		mainTP.getTabs().addAll(creAllinceTab);
+	}
+	
+	private void makeCreateMatch() {
+		createMatchTab creMatch = new createMatchTab();
+		Tab creMatchTab = new Tab("Create Match", creMatch);
+		mainTP.getTabs().addAll(creMatchTab);
+	}
+	
+	private void makeCreateAutoPlan() {
+		createAutoPlanTab creAutoPlan = new createAutoPlanTab();
+		Tab creAutoPlanTab = new Tab("Create Auto Plan", creAutoPlan);
+		mainTP.getTabs().addAll(creAutoPlanTab);
+	}
+	
+	private void makeCreateMatchPlan() {
+		
+	}
+	
+	private void makeOpenNewScDataMI() {
+		
+	}
+	
+	private void makeOpenNewPitDataMI() {
+		
+	}
+	
+	private void makeOpenNewDriveTeamDataMI() {
+		
+	}
+	
+	private void makeOpenNewDriveTeamComsMI() {
+		
+	}
+	
+	private void makeEditScDataMI() {
+		
+	}
+	
+	private void makeEditPitDataMI() {
+		
+	}
+	
+	private void makeEditDriveTeamDataMI() {
+		
+	}
+	
+	private void makeEditDriveTeamComsMI() {
+		
+	}
+	
+	private void makeExportScDataMI() {
+		
+	}
+	
+	private void makeExportPitDataMI() {
+		
+	}
+	
+	private void makeExportDriveTeamDataMI() {
+		
+	}
+	
+	
+	private void makeExportDriveTeamComsMI() {
+		
+	}
+	
+	@Override
+	public void handle(ActionEvent event) {
+		try {
+			if(event.getSource() == searchTeams) {
+				makeSearchTeams();
+			}
+			if(event.getSource() == searchAllince) {
+				makeSearchAllinces();
+			}
+			if(event.getSource() == searchAutos) {
+				makeSearchAutos();
+			}
+			if(event.getSource() == compareTeams) {
+				makeCompareTeams();
+			}
+			if(event.getSource() == compareAllince) {
+				makeCompareAllinces();
+			}
+			if(event.getSource() == compareAutos) {
+				makeCompareAutos();
+			}
+			if(event.getSource() == findBestTeam) {
+				makeFindBestTeam();
+			}
+			if(event.getSource() == findBestAllince) {
+				makeFindBestAllince();
+			}
+			if(event.getSource() == findBestAuto) {
+				makeFindBestAuto();
+			}
+			if(event.getSource() == findBestStat) {
+				makeFindBestStat();
+			}
+			if(event.getSource() == findBestRobotArchatype) {
+				makeFindBestRobotArchatype();
+			}
+			if(event.getSource() == createAllince) {
+				makeCreateAllince();
+			}
+			if(event.getSource() == createMatch) {
+				makeCreateMatch();
+			}
+			if(event.getSource() == createAutoplan) {
+				makeCreateAutoPlan();
+			}
+			if(event.getSource() == openNewScDataMI) {
+				makeCreateAutoPlan();
+			}
+			if(event.getSource() == openNewPitDataMI) {
+				makeCreateAutoPlan();
+			}
+			if(event.getSource() == openNewDriveTeamDataMI) {
+				makeCreateAutoPlan();
+			}
+			if(event.getSource() == openNewDriveTeamComsMI) {
+				makeCreateAutoPlan();
+			}
+			if(event.getSource() == editScDataMI) {
+				makeCreateAutoPlan();
+			}
+			if(event.getSource() == editPitDataMI) {
+				makeCreateAutoPlan();
+			}
+			if(event.getSource() == editDriveTeamDataMI) {
+				makeCreateAutoPlan();
+			}
+			if(event.getSource() == editDriveTeamComsMI) {
+				makeCreateAutoPlan();
+			}
+			if(event.getSource() == exportScDataMI) {
+				makeCreateAutoPlan();
+			}
+			if(event.getSource() == exportPitDataMI) {
+				makeCreateAutoPlan();
+			}
+			if(event.getSource() == exportDriveTeamDataMI) {
+				makeCreateAutoPlan();
+			}
+			if(event.getSource() == exportDriveTeamComsMI) {
+				makeCreateAutoPlan();
+			}
+		} catch(Exception e) {
+			System.out.println("error: " + e);
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+	
 }
