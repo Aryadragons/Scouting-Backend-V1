8a4bf1a9fd09888607970832d79d6484dc74ea6a
Arydragons <sunnybgp.icloud.com>
2024-07-04 10:19:00 -0500

    started Work on set PlayOff Alliances tab

---
+++ ScoutingBackendV1/src/application/setPlayOffAlliancesTab.java
@@ -0,0 +1,69 @@
+package application;
+
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class setPlayOffAlliancesTab extends GridPane{
+	
+	private ComboBox<String> team1CB;
+	private ComboBox<String> team2CB;
+	private ComboBox<String> team3CB;
+	private ComboBox<String> team4CB;
+	private ComboBox<String> team5CB;
+	private ComboBox<String> team6CB;
+	private ComboBox<String> team7CB;
+	private ComboBox<String> team8CB;
+	private ComboBox<String> team9CB;
+	private ComboBox<String> team10CB;
+	private ComboBox<String> team11CB;
+	private ComboBox<String> team12CB;
+	private ComboBox<String> team13CB;
+	private ComboBox<String> team14CB;
+	private ComboBox<String> team15CB;
+	private ComboBox<String> team16CB;
+	private ComboBox<String> team17CB;
+	private ComboBox<String> team18CB;
+	private ComboBox<String> team19CB;
+	private ComboBox<String> team20CB;
+	private ComboBox<String> team21CB;
+	private ComboBox<String> team22CB;
+	private ComboBox<String> team23CB;
+	private ComboBox<String> team24CB;
+
+	public setPlayOffAlliancesTab() {
+		this.add(new Label("Allince 1 Teams:   "), 0, 0);
+		this.add(new Label("Team 1:   "), 1, 6);
+		this.add(new Label("Team 2;   "), 2, 6);
+		this.add(new Label("Team 3:   "), 3, 6);
+		this.add(new Label("Allince 2 Teams:   "), 0, 2);
+		this.add(new Label("Team 1:   "), 1, 6);
+		this.add(new Label("Team 2;   "), 2, 6);
+		this.add(new Label("Team 3:   "), 3, 6);
+		this.add(new Label("Allince 3 Teams:   "), 0, 4);
+		this.add(new Label("Team 1:   "), 1, 6);
+		this.add(new Label("Team 2;   "), 2, 6);
+		this.add(new Label("Team 3:   "), 3, 6);
+		this.add(new Label("Allince 4 Teams:   "), 0, 6);
+		this.add(new Label("Team 1:   "), 1, 6);
+		this.add(new Label("Team 2;   "), 2, 6);
+		this.add(new Label("Team 3:   "), 3, 6);
+		this.add(new Label("Allince 5 Teams:   "), 0, 8);
+		this.add(new Label("Team 1:   "), 1, 8);
+		this.add(new Label("Team 2;   "), 2, 8);
+		this.add(new Label("Team 3:   "), 3, 8);
+		this.add(new Label("Allince 6 Teams:   "), 0, 10);
+		this.add(new Label("Team 1:   "), 1, 10);
+		this.add(new Label("Team 2;   "), 2, 10);
+		this.add(new Label("Team 3:   "), 3, 10);
+		this.add(new Label("Allince 7 Teams:   "), 0, 12);
+		this.add(new Label("Team 1:   "), 1, 12);
+		this.add(new Label("Team 2;   "), 2, 12);
+		this.add(new Label("Team 3:   "), 3, 12);
+		this.add(new Label("Allince 8 Teams:   "), 0, 14);
+		this.add(new Label("Team 1:   "), 1, 14);
+		this.add(new Label("Team 2;   "), 2, 14);
+		this.add(new Label("Team 3:   "), 3, 14);
+		
+	}
+}
